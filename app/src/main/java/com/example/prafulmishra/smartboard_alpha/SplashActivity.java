6e694d965861badbb2be42470b025d1acee0fcc3
PrafulMishra <prafulmishra0203@gmail.com>
2018-03-19 07:08:58 +0530

    WifiApState Failed

--- app/src/main/java/com/example/prafulmishra/smartboard_alpha/SplashActivity.java
+++ app/src/main/java/com/example/prafulmishra/smartboard_alpha/SplashActivity.java
@@ -1,17 +1,33 @@
 package com.example.prafulmishra.smartboard_alpha;
 
+        import android.Manifest;
+        import android.content.Context;
+        import android.content.pm.PackageManager;
+        import android.net.wifi.WifiConfiguration;
+        import android.net.wifi.WifiManager;
         import android.os.Bundle;
 
         import android.app.Activity;
         import android.content.Intent;
         import android.graphics.PixelFormat;
+        import android.support.v4.app.ActivityCompat;
+        import android.support.v4.content.ContextCompat;
+        import android.view.View;
         import android.view.Window;
         import android.view.animation.Animation;
         import android.view.animation.AnimationUtils;
+        import android.widget.Button;
         import android.widget.ImageView;
         import android.widget.LinearLayout;
+        import android.widget.Toast;
 
-public class SplashActivity extends Activity {
+        import java.lang.reflect.InvocationTargetException;
+        import java.lang.reflect.Method;
+
+
+public class SplashActivity extends Activity{
+    private static final int MY_PERMISSIONS_REQUEST_WIFI_STATE = 0;
+
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
         Window window = getWindow();
@@ -19,12 +35,57 @@ public class SplashActivity extends Activity {
     }
     /** Called when the activity is first created. */
     Thread splashTread;
+    WifiManager wifi_manager;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         StartAnimations();
-    }
+        wifi_manager = (WifiManager) this.getApplicationContext().getSystemService(WIFI_SERVICE);
+
+                // TODO Auto-generated method stub
+                wifi_manager.setWifiEnabled(false);
+
+                try
+                {
+                    //USE REFLECTION TO GET METHOD "SetWifiAPEnabled"
+                    Method method=wifi_manager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+                    WifiConfiguration netConfig = new WifiConfiguration();
+                    netConfig.SSID = "Smart";
+                    netConfig.preSharedKey = "smart123";
+                    netConfig.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+                    netConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+                    netConfig.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+                    netConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+                    netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+                    netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+                    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+                    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+                    method.invoke(netConfig, true);
+
+                }
+                catch (NoSuchMethodException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                catch (IllegalArgumentException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                catch (IllegalAccessException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                catch (InvocationTargetException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
     private void StartAnimations() {
         Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
         anim.reset();
